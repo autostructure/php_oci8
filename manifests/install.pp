4a0a93984e24b49d16f64727aec375c97220e223
Paul Talbot <paul@fullfrontalingenuity.com>
2016-05-26 15:26:28 -0400

    trying to remove force from package install
    Former-commit-id: 8c57447716d94991266fd1b777837cafabfe1e4b

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,13 +14,13 @@ class php_oci8::install {
 
   file {"${file_base_location}-devel-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm":
     ensure  => present,
-    replace => 'no',
+    #replace => 'no',
     source  => "puppet:///modules/php_oci8/oracle-instantclient${::php_oci8::major}.${::php_oci8::minor}-devel-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm",
   }
 
   file {"${::php_oci8::temp_location}/answers-pecl-oci8-${::php_oci8::major}.${::php_oci8::minor}.txt":
     ensure  => present,
-	replace => 'no',
+    #replace => 'no',
     source  => "puppet:///modules/php_oci8/answers-pecl-oci8-${php_oci8::major}.${::php_oci8::minor}.txt",
   }
 
@@ -29,7 +29,7 @@ class php_oci8::install {
     name            => "oracle-instantclient${::php_oci8::major}.${::php_oci8::minor}-basic-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm",
     provider        => 'rpm',
     source          => "${file_base_location}-basic-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm",
-    install_options => '--force',
+    #install_options => '--force',
     require         => File["${file_base_location}-basic-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm"],
   }
 
@@ -38,7 +38,7 @@ class php_oci8::install {
     name            => "oracle-instantclient${::php_oci8::major}.${::php_oci8::minor}-devel-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm",
     provider        => 'rpm',
     source          => "${file_base_location}-devel-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm",
-    install_options => '--force',
+    #install_options => '--force',
     require         => [ File["${file_base_location}-devel-${::php_oci8::version}-1.${::php_oci8::architecture}.rpm"], Package['oracle-instantclient-basic']],
   }
 
