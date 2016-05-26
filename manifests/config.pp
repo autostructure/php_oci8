ec1cf297a2e3a66e83f425226cb5338720ac06d0
Paul Talbot <paul@fullfrontalingenuity.com>
2016-05-26 16:21:00 -0400

    formatting changes in install.pp
    Former-commit-id: 35cf384988cab1ef337cdd77f5fdb6ac8851845d

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,40 +5,40 @@
 
 class php_oci8::config {
 
-
-    exec {'update pecl channel for pecl.php.net':
+  exec {'update pecl channel for pecl.php.net':
     command => '/bin/pecl channel-update pecl.php.net',
 	  user    => root,
 	  timeout => 0,
 	  tries   => 5,
 	  unless  => '/usr/bin/php -m | grep -c oci8',
 	  before  => Exec['pecl-install-oci8'],
-    }
-    exec {'pecl-install-oci8':
-      command => "/bin/pecl install oci8-${::php_oci8::pecl_oci8_version} </tmp/answers-pecl-oci8-${::php_oci8::major}.${::php_oci8::minor}.txt",
-      user    => root,
-      timeout => 0,
-      tries   => 5,
-      unless  => '/usr/bin/php -m | grep -c oci8',
-    }
+  }
 
-    file_line {'add-oci8-php':
-      path => '/etc/php.ini',
-      line => 'extension=oci8.so',
-    }
+  exec {'pecl-install-oci8':
+    command => "/bin/pecl install oci8-${::php_oci8::pecl_oci8_version} </tmp/answers-pecl-oci8-${::php_oci8::major}.${::php_oci8::minor}.txt",
+    user    => root,
+    timeout => 0,
+    tries   => 5,
+    unless  => '/usr/bin/php -m | grep -c oci8',
+  }
 
-    file_line {'env-oracle-home':
-      path   => '/etc/environment',
-      line   => "export ORACLE_HOME=/usr/lib/oracle/${::php_oci8::major}.${::php_oci8::minor}/client64/lib",
-      match  => '^export\ ORACLE_HOME\=',
-      notify => Service['httpd'],
-    }
+  file_line {'add-oci8-php':
+    path => '/etc/php.ini',
+    line => 'extension=oci8.so',
+  }
 
-    file_line {'env-oracle-nls-date-format':
-      path   => '/etc/environment',
-      line   => "export NLS_DATE_FORMAT=\"DD/MM/YYYY HH24:MI\"",
-      match  => '^export\ NLS_DATE_FORMAT\=',
-      notify => Service['httpd'],
-    }
+  file_line {'env-oracle-home':
+    path   => '/etc/environment',
+    line   => "export ORACLE_HOME=/usr/lib/oracle/${::php_oci8::major}.${::php_oci8::minor}/client64/lib",
+    match  => '^export\ ORACLE_HOME\=',
+    notify => Service['httpd'],
+  }
+
+  file_line {'env-oracle-nls-date-format':
+    path   => '/etc/environment',
+    line   => "export NLS_DATE_FORMAT=\"DD/MM/YYYY HH24:MI\"",
+    match  => '^export\ NLS_DATE_FORMAT\=',
+    notify => Service['httpd'],
+  }
 
 }
