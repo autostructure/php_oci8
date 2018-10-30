e0629df63073a49fb2835a02017294e98aa7f09c
Paul Talbot <prolixalias@gmail.com>
2018-10-30 09:36:54 -0400

    Testing with array starting at 0

--- lib/facter/pecl_oci8_extension.rb
+++ lib/facter/pecl_oci8_extension.rb
@@ -14,9 +14,9 @@ if File.file? '/bin/pecl'
 
   if pecl_output_array.length > 1
     pecl_oci8_extension['version'] = {
-      'major' => pecl_output_array[1].to_i,
-      'minor' => pecl_output_array[2].to_i,
-      'patch' => pecl_output_array[3].to_i,
+      'major' => pecl_output_array[0].to_i,
+      'minor' => pecl_output_array[1].to_i,
+      'patch' => pecl_output_array[2].to_i,
       'full'  => pecl_output.to_s,
       #'full' => "#{df_output_array[1]}.#{df_output_array[2]}.#{df_output_array[3]}"
     }
